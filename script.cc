f065152b414f6e822377760a595ea0544c5e045e
xbilek26 <bilekf@email.cz>
2024-12-23 19:57:45 +0100

    update script.cc

--- script.cc
+++ script.cc
@@ -1,79 +1,30 @@
-/*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation;
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
-#include "ns3/applications-module.h"
 #include "ns3/core-module.h"
 #include "ns3/internet-module.h"
-#include "ns3/network-module.h"
-#include "ns3/point-to-point-module.h"
-
-// Default Network Topology
-//
-//       10.1.1.0
-// n0 -------------- n1
-//    point-to-point
-//
+#include "ns3/lte-helper.h"
+#include "ns3/point-to-point-epc-helper.h"
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE("FirstScriptExample");
+NS_LOG_COMPONENT_DEFINE("kpm_project");
 
-int
-main(int argc, char* argv[])
-{
-    CommandLine cmd(__FILE__);
-    cmd.Parse(argc, argv);
+int main(int argc, char *argv[]) {
+    
+    // At least two eNodeBs.
+    uint16_t numENodeBs = 2;
 
-    Time::SetResolution(Time::NS);
-    LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
-    LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+    // A minimum of five User Equipment (UE) devices.
+    uint16_t numUEs = 5;
 
-    NodeContainer nodes;
-    nodes.Create(2);
+    // At least one remote host (server) on the Internet.
+    uint16_t numRemoteHosts = 1;
 
-    PointToPointHelper pointToPoint;
-    pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
-    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+    NodeContainer ueNodes;
+    NodeContainer eNodeBNodes;
+    NodeContainer remoteHostContainer;
 
-    NetDeviceContainer devices;
-    devices = pointToPoint.Install(nodes);
+    eNodeBNodes.Create(numENodeBs);
+    ueNodes.Create(numUEs);
+    remoteHostContainer.Create(numRemoteHosts);
 
-    InternetStackHelper stack;
-    stack.Install(nodes);
-
-    Ipv4AddressHelper address;
-    address.SetBase("10.1.1.0", "255.255.255.0");
-
-    Ipv4InterfaceContainer interfaces = address.Assign(devices);
-
-    UdpEchoServerHelper echoServer(9);
-
-    ApplicationContainer serverApps = echoServer.Install(nodes.Get(1));
-    serverApps.Start(Seconds(1.0));
-    serverApps.Stop(Seconds(10.0));
-
-    UdpEchoClientHelper echoClient(interfaces.GetAddress(1), 9);
-    echoClient.SetAttribute("MaxPackets", UintegerValue(1));
-    echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
-    echoClient.SetAttribute("PacketSize", UintegerValue(1024));
-
-    ApplicationContainer clientApps = echoClient.Install(nodes.Get(0));
-    clientApps.Start(Seconds(2.0));
-    clientApps.Stop(Seconds(10.0));
-
-    Simulator::Run();
-    Simulator::Destroy();
     return 0;
 }
