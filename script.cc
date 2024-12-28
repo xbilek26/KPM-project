4d9d677a025dbbb0acc807c19a37e1a5403fc3e3
xbilek26 <bilekf@email.cz>
2024-12-29 00:31:17 +0100

    update script.cc

--- script.cc
+++ script.cc
@@ -5,9 +5,10 @@
  * ...
  * ...
  * 
- * 
- * TODO: Mobility
- * 
+ * TODO: Simulation Run
+ * - Execute the simulation with different sets of parameters and collect data on network performance.
+ * - Run the simulation for a sufficient duration to observe network behavior.
+ * - Capture and analyze key metrics, such as throughput, latency, and packet loss.
  */
 
 #include "ns3/applications-module.h"
@@ -25,24 +26,30 @@
 /* ---------------------------- SIMULATION SCENARIO ----------------------------
  *
  *                           +----------------------+
- *                           | Remote Host 10.0.0.1 |
+ *                           |      Remote Host     |
  *                           +----------------------+
+ *                               1.0.0.1 |
+ *                                       | Point-to-Point
  *                                       |
- *                                       | Point-to-Point (Internet)
- *                                       |
- *                                       v
+ *                               1.0.0.2 v
  *                           +----------------------+
- *                           |     PGW 10.0.0.2     |
+ *                           |         P-GW         |
  *                           +----------------------+
+ *                              14.0.0.5 |
+ *                                       | S5 interface
+ *                              14.0.0.6 |
+ *                           +----------------------+ 13.0.0.6     13.0.0.5 +----------------------+
+ *                           |         S-GW         |-----------------------|         MME          |
+ *                           +----------------------+     S11 interface     +----------------------+
  *                                       |
- *                                       | (S1 interface)
+ *                                       |
  *                                       |
  *                       +---------------+----------------+
  *                       |                                |
  *                       v                                v
- *           +------------------------+       +------------------------+
- *           |        eNodeB 0        |       |        eNodeB 1        |
- *           +------------------------+       +------------------------+
+ *           +-----------------------+        +-----------------------+
+ *           |   eNodeB 0 10.0.0.5   |        |   eNodeB 1 10.0.0.9   |
+ *           +-----------------------+        +-----------------------+
  *                       |                                |
  *                       | LTE                            | LTE
  *                       v                                v
@@ -72,7 +79,6 @@ int main(int argc, char *argv[])
     uint32_t numUes = 5;
 
     // Simulation parameters
-    double distance = 500.0; // Distance between eNodeBs
     double simTime = 10.0;
 
     // Create LTE Helper and EPC Helper
@@ -93,22 +99,26 @@ int main(int argc, char *argv[])
     InternetStackHelper internet;
     internet.Install(remoteHostContainer);
 
-    // Set up point-to-point link between PGW and remote host
+    // Set up point-to-point link between P-GW and remote host
     PointToPointHelper p2p;
     p2p.SetDeviceAttribute("DataRate", StringValue("100Gbps"));
     p2p.SetChannelAttribute("Delay", StringValue("10ms"));
     Ptr<Node> pgw = epcHelper->GetPgwNode();
     NetDeviceContainer internetDevices = p2p.Install(remoteHost, pgw);
 
+    // Get S-GW and MME nodes (just for NetAnim configuration)
+    Ptr<Node> sgw = epcHelper->GetSgwNode();
+    Ptr<Node> mme = NodeList::GetNode(2); // No getter method for MME
+
     // Enable pcap tracing on point-to-point link
     p2p.EnablePcapAll("p2p");
 
     // Assign IP addresses to point-to-point link (internet)
     Ipv4AddressHelper ipv4hInternet;
-    ipv4hInternet.SetBase("10.0.0.0", "255.255.255.0");
+    ipv4hInternet.SetBase("1.0.0.0", "255.0.0.0");
     Ipv4InterfaceContainer internetIpIfaces = ipv4hInternet.Assign(internetDevices);
     NS_LOG_INFO("Remote host address: " << internetIpIfaces.GetAddress(0));
-    NS_LOG_INFO("PGW address: " << internetIpIfaces.GetAddress(1));
+    NS_LOG_INFO("P-GW address: " << internetIpIfaces.GetAddress(1));
 
     // Create static routing
     Ipv4StaticRoutingHelper ipv4RoutingHelper;
@@ -120,8 +130,7 @@ int main(int argc, char *argv[])
     ueInternet.Install(ueNodes);
 
     // Mobility
-
-    MobilityHelper mobilityUe;
+    MobilityHelper mobilityUes;
     MobilityHelper mobilityEnbs;
 
     mobilityEnbs.SetPositionAllocator(
@@ -134,18 +143,18 @@ int main(int argc, char *argv[])
 
     mobilityEnbs.SetMobilityModel("ns3::ConstantPositionMobilityModel");
 
-    mobilityUe.SetPositionAllocator(
+    mobilityUes.SetPositionAllocator(
         "ns3::RandomRectanglePositionAllocator",
         "X", StringValue("ns3::UniformRandomVariable[Min=0|Max=100]"),
         "Y", StringValue("ns3::UniformRandomVariable[Min=0|Max=100]")
     );
     
-    mobilityUe.SetMobilityModel("ns3::RandomWalk2dMobilityModel",
+    mobilityUes.SetMobilityModel("ns3::RandomWalk2dMobilityModel",
     "Bounds", RectangleValue(Rectangle(0, 100, 0, 100)),
     "Speed", StringValue("ns3::ConstantRandomVariable[Constant=10.0]")
     );
 
-    mobilityUe.Install(ueNodes);
+    mobilityUes.Install(ueNodes);
     mobilityEnbs.Install(enbNodes);
 
     NetDeviceContainer enbLteDevs = lteHelper->InstallEnbDevice(enbNodes); // Add eNB nodes to the container
@@ -206,68 +215,45 @@ int main(int argc, char *argv[])
         sinkApps.Add(sinkHelper.Install(ueNodes.Get(i)));
     }
 
-    sourceApps.Start(Seconds(0.0));
-    sinkApps.Start(Seconds(1.0));
+    sourceApps.Start(Seconds(1.0));
+    sinkApps.Start(Seconds(2.0));
     sourceApps.Stop(Seconds(10.0));
     sinkApps.Stop(Seconds(9.0));
 
     // Flow monitor
     Ptr <FlowMonitor> monitor;
     FlowMonitorHelper flowMonHelper;
-    monitor = flowMonHelper.InstallAll();  // Install all nodes in one monitor
-    monitor->Start(Seconds(1.0));  // Optional: start monitoring after some time
-
-    // Po skončení simulace
-    Simulator::Stop(Seconds(simTime));
-    Simulator::Run();
-    monitor->CheckForLostPackets();
-    Ptr <Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier>(flowMonHelper.GetClassifier());
-    std::map <FlowId, FlowMonitor::FlowStats> stats = monitor->GetFlowStats();
-
-    std::cout << std::endl << "******* Flow monitor statistics *******" << std::endl;
-    for (std::map<FlowId, FlowMonitor::FlowStats>::const_iterator i = stats.begin(); i != stats.end(); i++) {
-        Ipv4FlowClassifier::FiveTuple t = classifier->FindFlow(i->first);
-        std::cout << "Flow ID: " << i->first << std::endl;
-        std::cout << "Src add: " << t.sourceAddress << "-> Dst add: " << t.destinationAddress << std::endl;
-        std::cout << "Src port: " << t.sourcePort << "-> Dst port: " << t.destinationPort << std::endl;
-        std::cout << "Tx Packets/Bytes: " << i->second.txPackets << "/" << i->second.txBytes << std::endl;
-        std::cout << "Rx Packets/Bytes: " << i->second.rxPackets << "/" << i->second.rxBytes << std::endl;
-        std::cout << "Throughput: " << i->second.rxBytes * 8.0 / (i->second.timeLastRxPacket.GetSeconds() - i->second.timeFirstTxPacket.GetSeconds()) / 1024 << "kb/s" << std::endl;
-        std::cout << "Delay sum: " << i->second.delaySum.GetMilliSeconds() << "ms" << std::endl;
-        std::cout << "Mean delay: " << (i->second.delaySum.GetSeconds() / i->second.rxPackets) * 1000 << "ms" << std::endl;
-        std::cout << "Jitter sum: " << i->second.jitterSum.GetMilliSeconds() << "ms" << std::endl;
-        std::cout << "Mean jitter: " << (i->second.jitterSum.GetSeconds() / (i->second.rxPackets - 1)) * 1000 << "ms" << std::endl;
-        std::cout << "Lost Packets: " << i->second.txPackets - i->second.rxPackets << std::endl;
-        std::cout << "Packet loss: " << (((i->second.txPackets - i->second.rxPackets) * 1.0) / i->second.txPackets) * 100 << "%" << std::endl;
-        std::cout << "------------------------------------------------" << std::endl;
-    }
+    monitor = flowMonHelper.InstallAll();
+    monitor->Start(Seconds(1.0));
 
     // NetAnim configuration
-
-    AnimationInterface::SetConstantPosition(remoteHost, 25.0, 0.0);
-    AnimationInterface::SetConstantPosition(pgw, 50.0, 0.0);
+    AnimationInterface::SetConstantPosition(remoteHost, 50.0, 5.0);
+    AnimationInterface::SetConstantPosition(pgw, 50.0, 20.0);
+    AnimationInterface::SetConstantPosition(sgw, 50.0, 35.0);
+    AnimationInterface::SetConstantPosition(mme, 75.0, 35.0);
     AnimationInterface anim("lte-simulation.xml");
     anim.EnablePacketMetadata(true);
-    anim.SetMobilityPollInterval(Seconds(0.1));
-
-    anim.SetConstantPosition(remoteHost, 50.0, 10.0);
-    anim.SetConstantPosition(pgw, 50.0, 30.0);
+    anim.SetMobilityPollInterval(Seconds(0.05));
 
     unsigned long long testValue = 0xFFFFFFFFFFFFFFFF;
     anim.SetMaxPktsPerTraceFile(testValue);
 
-    anim.UpdateNodeDescription(remoteHost, "RemoteHost");
-    anim.UpdateNodeDescription(pgw, "PGW");
-    anim.UpdateNodeDescription(enbNodes.Get(0), "eNodeB_0");
-    anim.UpdateNodeDescription(enbNodes.Get(1), "eNodeB_1");
-    anim.UpdateNodeDescription(ueNodes.Get(0), "UE_0");
-    anim.UpdateNodeDescription(ueNodes.Get(1), "UE_1");
-    anim.UpdateNodeDescription(ueNodes.Get(2), "UE_2");
-    anim.UpdateNodeDescription(ueNodes.Get(3), "UE_3");
-    anim.UpdateNodeDescription(ueNodes.Get(4), "UE_4");
+    anim.UpdateNodeDescription(remoteHost, "Remote Host");
+    anim.UpdateNodeDescription(pgw, "P-GW");
+    anim.UpdateNodeDescription(sgw, "S-GW");
+    anim.UpdateNodeDescription(mme, "MME");
+    anim.UpdateNodeDescription(enbNodes.Get(0), "eNodeB 0");
+    anim.UpdateNodeDescription(enbNodes.Get(1), "eNodeB 1");
+    anim.UpdateNodeDescription(ueNodes.Get(0), "UE 0");
+    anim.UpdateNodeDescription(ueNodes.Get(1), "UE 1");
+    anim.UpdateNodeDescription(ueNodes.Get(2), "UE 2");
+    anim.UpdateNodeDescription(ueNodes.Get(3), "UE 3");
+    anim.UpdateNodeDescription(ueNodes.Get(4), "UE 4");
 
     anim.UpdateNodeColor(remoteHost, 255, 0, 0);
     anim.UpdateNodeColor(pgw, 255, 255, 0);
+    anim.UpdateNodeColor(sgw, 0, 255, 255);
+    anim.UpdateNodeColor(mme, 255, 0, 255);
     anim.UpdateNodeColor(enbNodes.Get(0), 0, 255, 0);
     anim.UpdateNodeColor(enbNodes.Get(1), 0, 255, 0);
     anim.UpdateNodeColor(ueNodes.Get(0), 0, 0, 255);
@@ -278,6 +264,8 @@ int main(int argc, char *argv[])
 
     anim.UpdateNodeSize(remoteHost->GetId(), 5.0, 5.0);
     anim.UpdateNodeSize(pgw->GetId(), 5.0, 5.0);
+    anim.UpdateNodeSize(sgw->GetId(), 5.0, 5.0);
+    anim.UpdateNodeSize(mme->GetId(), 5.0, 5.0);
     anim.UpdateNodeSize(enbNodes.Get(0)->GetId(), 5.0, 5.0);
     anim.UpdateNodeSize(enbNodes.Get(1)->GetId(), 5.0, 5.0);
     anim.UpdateNodeSize(ueNodes.Get(0)->GetId(), 2.0, 2.0);
@@ -288,6 +276,28 @@ int main(int argc, char *argv[])
 
     Simulator::Stop(Seconds(simTime));
     Simulator::Run();
+
+    monitor->CheckForLostPackets();
+    Ptr <Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier>(flowMonHelper.GetClassifier());
+    std::map <FlowId, FlowMonitor::FlowStats> stats = monitor->GetFlowStats();
+
+    std::cout << std::endl << "************* FLOW MONITOR STATISTICS *************" << std::endl;
+    for (std::map<FlowId, FlowMonitor::FlowStats>::const_iterator i = stats.begin(); i != stats.end(); i++) {
+        Ipv4FlowClassifier::FiveTuple t = classifier->FindFlow(i->first);
+        std::cout << "Flow ID: " << i->first << std::endl;
+        std::cout << "Src address: " << t.sourceAddress << " -> Dst address: " << t.destinationAddress << std::endl;
+        std::cout << "Src port: " << t.sourcePort << " -> Dst port: " << t.destinationPort << std::endl;
+        std::cout << "Tx Packets/Bytes: " << i->second.txPackets << "/" << i->second.txBytes << std::endl;
+        std::cout << "Rx Packets/Bytes: " << i->second.rxPackets << "/" << i->second.rxBytes << std::endl;
+        std::cout << "Throughput: " << i->second.rxBytes * 8.0 / (i->second.timeLastRxPacket.GetSeconds() - i->second.timeFirstTxPacket.GetSeconds()) / 1024 << " kb/s" << std::endl;
+        std::cout << "Delay sum: " << i->second.delaySum.GetMilliSeconds() << " ms" << std::endl;
+        std::cout << "Mean delay: " << (i->second.delaySum.GetSeconds() / i->second.rxPackets) * 1000 << " ms" << std::endl;
+        std::cout << "Jitter sum: " << i->second.jitterSum.GetMilliSeconds() << " ms" << std::endl;
+        std::cout << "Mean jitter: " << (i->second.jitterSum.GetSeconds() / (i->second.rxPackets - 1)) * 1000 << " ms" << std::endl;
+        std::cout << "Lost Packets: " << i->second.txPackets - i->second.rxPackets << std::endl;
+        std::cout << "Packet loss: " << (((i->second.txPackets - i->second.rxPackets) * 1.0) / i->second.txPackets) * 100 << " %" << std::endl;
+        std::cout << "------------------------------------------------" << std::endl;
+    }
 
     Simulator::Destroy();
 
